e1cfe51c3a557c5bbe9f09d669effdf116651218
PrasannaGodiyal3903 <godiyalprasanna16@gmail.com>
2026-01-06 00:01:47 +0530

    Add user dictionary with roles and passwords

---
+++ users.py
@@ -0,0 +1,6 @@
+users = {
+    "admin": {"password": "admin123", "role": "ADMIN"},
+    "hr1": {"password": "hr123", "role": "HR"},
+    "eng1": {"password": "eng123", "role": "ENGINEER"},
+    "intern1": {"password": "intern123", "role": "INTERN"},
+}
